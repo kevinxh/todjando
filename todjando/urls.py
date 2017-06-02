31a0ff26c9ed77a747ce7113c4c034134c4ef1b9
Kevin He <khe@mobify.me>
2017-06-01 22:24:31 -0700

    view:helloworld

--- todjando/urls.py
+++ todjando/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from todo.views import hello_world
 
 urlpatterns = [
+    url(r'^$', hello_world),
     url(r'^admin/', admin.site.urls),
 ]
